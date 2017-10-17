b9db478af65637c0f21df45c793939b055b72ce5
Daniele Campogiani <daniele.campogiani@gmail.com>
2017-10-17 14:43:38 +0200

    Try Demo

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,17 @@
 import datatypes.OptionDemo
+import datatypes.TryDemo
+import kategory.Either
+import kategory.Try
 
 fun main(args: Array<String>) {
 
-    OptionDemo()
+
+    val success = TryDemo.parseInt("42").map { TryDemo.multuplyBy2(it) }
+    val failure = TryDemo.parseInt("pippo").map { TryDemo.multuplyBy2(it) }
+
+    Try
+
+    print(success)
+    print(failure)
+
 }
