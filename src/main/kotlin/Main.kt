21072aaa2df278533f969e2af20fed9859725b63
Daniele Campogiani <daniele.campogiani@gmail.com>
2017-11-20 21:26:16 +0100

    Use .ev() extension methods to get data type back

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,7 @@
-import kategory.*
+import kategory.Option
+import kategory.Try
+import kategory.ev
+import kategory.some
 import typeclasses.FunctorDemo
 
 fun main(args: Array<String>) {
@@ -7,8 +10,8 @@ fun main(args: Array<String>) {
     val some10 = 10.some()
     val success10 = 10.success()
 
-    val optionFun = FunctorDemo.multiplyBy2<OptionHK>(some10)
-    val tryFun = FunctorDemo.multiplyBy2<TryHK>(success10)
+    val optionFun: Option<Int> = FunctorDemo.multiplyBy2(some10).ev()
+    val tryFun: Try<Int> = FunctorDemo.multiplyBy2(success10).ev()
 
     println("Option : $optionFun")
     println("Try : $tryFun")
