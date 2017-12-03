25faddab04a8e0799b08442a1c4c6e2c95c10629
Daniele Campogiani <daniele.campogiani@gmail.com>
2017-12-03 20:13:37 +0100

    Validate with Option: which one is the wrong one?

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,16 @@
-import typeclasses.ApplicativeDemo
+import validation.Data
+import validation.option.OptionValidation
 
 fun main(args: Array<String>) {
 
-    ApplicativeDemo()
+    val first = OptionValidation.validateData(Data("parenzio@gmail.com", "1234567872"))
+    val second = OptionValidation.validateData(Data("parenzio.com", "1234567872"))
+    val third = OptionValidation.validateData(Data("parenzio@gmail.com", "23"))
+    val fourth = OptionValidation.validateData(Data("parenzio.com", "23"))
+
+    println(first)
+    println(second)
+    println(third)
+    println(fourth)
 }
 
